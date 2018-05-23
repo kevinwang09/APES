5c9b0b7f9fb1d2a77fbac5e2f2d5470975dc61aa
kevinwang09 <kevin.wang@sydney.edu.au>
2018-05-23 18:03:38 +1000

    apes_mio now available

--- R/apes_mio.R
+++ R/apes_mio.R
@@ -1,32 +1,33 @@
-#' Perform APES using the leaps package
+#' Perform APES using the bestsubset package
 #' @param x n times p design matrix for logistics regression
 #' @param y response vector for logistics regression of length n
 #' @param Pi esimated probability of each observation using a baseline model. Of length n.
-#' @param maxK maximum model size, less than p.
-#' @import leaps
+#' @param krange range of k to explore
+#' @import bestsubset
 #' @import tibble
 #' @import broom
 #' @import magrittr
 #' @import dplyr
 #' @export
 #' @examples
-#'
 #' set.seed(10)
 #' n = 100
 #' p = 10
-#' maxK = 5
+#' krange = 1:5
 #
 #' x = matrix(rnorm(n*p), ncol = p)
 #' colnames(x) = paste0("X", 1:p)
 #' y = round(runif(n, 0,1))
 #' Pi = runif(n, 0, 1)
-#' apesResult = apes_leaps(x = x, y = y, Pi = Pi, maxK = maxK)
+#' apesResult = apes_mio(x = x, y = y, Pi = Pi, krange = krange)
 #' names(apesResult)
 
-apes_leaps = function(x,
-                      y,
-                      Pi,
-                      maxK){
+
+apes_mio = function(x,
+                    y,
+                    Pi,
+                    krange,
+                    time.limit = 100){
 
   variables = c("Int", colnames(x))
   x = x
@@ -39,12 +40,12 @@ apes_leaps = function(x,
 
   ############# Begin apes solver ##############
   apesT1 = Sys.time()
-
-  apesRes = leaps::regsubsets(x = x, y = linearY,
-                              really.big = T,
-                              nbest = 1,
-                              nvmax = maxK,
-                              method = "exhaustive")
+  apesRes = apesSolver_mio(x = x,
+                           y = linearY,
+                           # k = krange
+                           k = krange,
+                           time.limit = time.limit
+  )
   apesT2 = Sys.time()
   print("Finished")
   ## Something is very wrong with the intercept term of apes, so we will call this version as apesHosmer
@@ -56,11 +57,9 @@ apes_leaps = function(x,
   ### For APES
   ## Authors scaled the model so that there is no intercept term of apes, so we will call this version as apesHosmer
   ## We will construct apesMle based on the indicator functions
-  summaryApesRes = summary(apesRes)
-
-  apesIndicator = t(summaryApesRes$which[, -1])
-  rownames(apesIndicator) = variables[-1]
-  apesModelSize = colSums(apesIndicator) ## We always include intercept term!!
+  apesHosmerBeta = apesRes$beta
+  apesIndicator = (apesHosmerBeta != 0)
+  apesModelSize = colSums(apesIndicator) + 1L ## We always include intercept term!!
   apesModelName = paste0("apesModel", apesModelSize)
 
 
@@ -80,14 +79,14 @@ apes_leaps = function(x,
   ############# End Exact Logistic solver ##############
 
   ############# Constructing model information ##################
-  apesModelDf = tibble::tibble(
+  apesModelDf = tibble(
     method = "apes",
     modelName = apesModelName,
     modelSize = apesModelSize,
     apesMleLoglike = apesMleLoglike,
     mleAIC = apesMleAIC,
     mleBIC = apesMleBIC,
-    status = "leaps_optimal",
+    status = apesRes$status,
     icOptimalModels = paste0(
       icOptimal(ic = apesMleAIC, "apesMinAic"),
       icOptimal(ic = apesMleBIC, "apesMinBic")
@@ -107,7 +106,7 @@ apes_leaps = function(x,
   apesMinBicProb = minIcMatrix(ic = apesMleBIC,
                                mat = apesMlePi)
   ############
-  responseTibble = tibble::tibble(obsNum = obsNum,
+  responseTibble = data.frame(obsNum = obsNum,
                               y = y,
                               Pi = Pi,
                               linearY = linearY,
@@ -138,77 +137,18 @@ apes_leaps = function(x,
   )
   return(result)
 }
-####################################################################################
-expit = function(x){
-  1/(1+exp(-x))
-}
 
-logit = function(x){
-  log(x) - log(1-x)
-}
-##############################
-## If a vector of IC has a minimum, then returns a vector with that minimum labelled as symbol.
-icOptimal = function(ic, symbol){
-  if(length(which.min(ic)) == 0){
-    res = NA
-  } else {
-    res = rep("", length(ic))
-    res[which.min(ic)] = symbol
-  }
-  return(res)
-}
-##############################
-minIcMatrix = function(ic, mat){
-  if(length(which.min(ic)) == 0){
-    res = NA
-  } else {
-    res = mat[,which.min(ic)]
-  }
-  return(res)
-}
-####################################
-## Calculate the log-likelihood of logisitic regression using yBinom and estimated pis
-loglikePi = function(yBinom, pis){
-  loglike = yBinom*log(pis) + (1-yBinom)*log(1-pis)
-  return(sum(loglike))
-}
-#####################################
-## Calculate estimated pis using beta
-beta2Pi = function(X, beta){
-  xint = cbind(Int = 1,X)
-  expit(xint[, names(beta)] %*% as.matrix(beta))
-}
-#####################################
-#####################################
-## Given an indicators of variables, design matrix and the yBinom, we refit the MLE-logisitic. X should not have an Intercept term
-refittingMle = function(indicator, X, yBinom){
-  xTemp = cbind(Int = 1, X[,indicator])
-  colnames(xTemp) = c("Int", colnames(X)[indicator])
 
-  glm.fit(x = xTemp,
-          y = yBinom,
-          # etastart = fullModel$linear.predictors,
-          family = binomial(link = "logit"))
-}
-#####################################
-## We take the MLE model fitted above and rearrange the columns and remove the NA's in our vector
-mleModelToBeta = function(mleModels, variables){
-  mleBeta = purrr::map(mleModels, "coefficients") %>%
-    purrr::map(t) %>%
-    purrr::map(data.frame) %>%
-    dplyr::bind_rows() %>% t
-
-  mleBeta[is.na(mleBeta)] = 0L
-  variablesOrdered = intersect(rownames(mleBeta), variables) %>% gtools::mixedsort()
-  # variablesOrdered = variables %>% mixedsort
-  mleBeta = mleBeta[variablesOrdered,]
-
-  tmp = matrix(0L,
-               nrow = length(variables),
-               ncol = ncol(mleBeta),
-               dimnames = list(variables, colnames(mleBeta))) ## To avoid a variable is never selected, we create an empty matrix
-
-  tmp[rownames(mleBeta), ] = mleBeta
-
-  return(tmp)
+## Default apes solver
+apesSolver_mio = function(x, y, k = 1:min(nrow(x), ncol(x)), time.limit){
+  apes.obj = bestsubset::bs(x = x,
+                            y = y,
+                            intercept = T,
+                            k = k,
+                            verbose = TRUE,
+                            tol = 1e-6,
+                            form = 1,
+                            nruns = 50,
+                            time.limit = time.limit)
+  return(apes.obj)
 }
